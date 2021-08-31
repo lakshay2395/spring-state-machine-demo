94dd260f7aa106983ba15dee734c25a662dcfd66
Lakshay Bhambri <lakshaybhambri@NOVI-C02G229GMD6M.local>
2021-08-31 15:08:22 +0530

    Initial Commit

---
+++ src/test/java/com/springstatemachinedemo/SpringStateMachineDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springstatemachinedemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringStateMachineDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
